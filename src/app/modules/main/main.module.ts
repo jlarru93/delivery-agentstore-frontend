56bce26ee0f5263d22c9794fd3bd9c427c5edaf7
jhon larru <jhon.larru@siedevs.com>
2023-04-23 08:42:53 -0500

    task main

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,15 +3,19 @@ import {AgmCoreModule} from '../../../agm/core';
 import { CommonModule } from "@angular/common";
 import { MainComponent } from "./main.component";
 import { NgModule } from "@angular/core";
+import { MainRoutingModule } from "./main-routing.module";
+import { CardModule } from "primeng/card";
 
 @NgModule({
     declarations: [MainComponent],
     imports: [
       CommonModule,
+      MainRoutingModule,
       AgmCoreModule.forRoot({
         apiKey: "AIzaSyDl_VavzdtyqvdrsOy3Mnsg9hFusgMZ_SY",
         libraries: ['drawing']
-      })
+      }),
+      CardModule
     ],
     providers:[MainService]
   })
