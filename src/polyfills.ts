28c597cc4b25dbbd9118cf0815a314a71df7a547
jhon larru <jhon.larru@siedevs.com>
2023-04-28 16:43:26 -0500

    buffer

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -62,7 +62,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 (window as any)['global'] = window;
-
+global.Buffer = global.Buffer || require('buffer').Buffer;
 (window as any).process = {
    env: { DEBUG: undefined },
  };
