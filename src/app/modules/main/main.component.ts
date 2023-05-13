a5d8db0dc7b0a21e6208890485362709fb56e1f2
jhon larru <jhon.larru@siedevs.com>
2023-05-13 15:06:31 -0500

    pasa de estado en estado y recepciona pedidos por mqtt

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -38,6 +38,7 @@ import { StoreHandler } from "../service/handlers/store.handler";
 
     constructor(public dialogService: DialogService,private productService: ProductService,private orderService:OrderService,private mqtt:MqttService,private orderHandler:OrderHandler,private storeHandler:StoreHandler){}
     ngOnInit(): void {
+      console.log("MAIN")
       this.productService.getProductsWithOrdersSmall().then(data => this.products = data);
       this.getOrders()
       this.mqtt._onConnect.subscribe((isConnect)=>{
@@ -51,7 +52,7 @@ import { StoreHandler } from "../service/handlers/store.handler";
 
     validOrdersSubscribe(){
       if(this.isMqttConnect && this.isDoneGetOrders){
-        this.orders.forEach((order)=>this.mqtt.subscribe("order/"+order.uuid))
+        this.orders.forEach((order)=>this.subscribeOrder(order.uuid))
       }
     }
 
@@ -67,16 +68,18 @@ import { StoreHandler } from "../service/handlers/store.handler";
       this.orderHandler._data.subscribe((asyncData)=>{
         if(asyncData){
           let orderMqtt=OrderResponse.toBean(asyncData.data)
-          console.log("orderMqtt",orderMqtt)
-          let order=this.orders.find((order)=>order.id === orderMqtt.id)
-          console.log("order",order)
-          order=orderMqtt
+          let orderIndex=this.orders.findIndex((order)=>order.id === orderMqtt.id)
+          this.orders[orderIndex]=orderMqtt
+          console.log(orderMqtt)
           this.sortOrders()
         }
       })
-      this.storeHandler._data.subscribe((data)=>{
-        if(data){
-          console.log("ORDER recibida",data)
+      this.storeHandler._data.subscribe((asyncData)=>{
+        if(asyncData){
+          let orderMqtt=OrderResponse.toBean(asyncData.data)
+          this.orders.push(orderMqtt)
+          this.sortOrders()
+          this.subscribeOrder(orderMqtt.uuid)
         }
       })
     }
--- src/app/modules/service/handlers/store.handler.ts
+++ src/app/modules/service/handlers/store.handler.ts
@@ -1,16 +1,20 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
+import { AsyncData } from "../data/response";
+import { OrderResponse } from "../../main/service/data/response";
 
 @Injectable({
     providedIn: 'root'
 })
 export class StoreHandler{
-    public _data: BehaviorSubject<string> = new BehaviorSubject<string>(null);
+    public _data: BehaviorSubject<AsyncData<OrderResponse>> = new BehaviorSubject<AsyncData<OrderResponse>>(null);
     data$ = this._data.asObservable();
 
     handle(payload: string) {
         console.log("StoreHandler",payload)
-        this._data.next(payload)
+        let response=JSON.parse(payload) as AsyncData<OrderResponse>
+        this._data.next(response)
+        
         
     }
 }
