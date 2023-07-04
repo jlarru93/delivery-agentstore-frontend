7eb39314f04289e7194710aa73aabe5c4a15369e
developer externo <developerexterno@gmail.com>
2023-07-04 14:30:33 -0500

    agregando pull request

--- src/app/modules/service/mqtt.service.ts
+++ src/app/modules/service/mqtt.service.ts
@@ -15,7 +15,6 @@ export class MqttService {
     client: Client
     message: string = ""
     constructor(private routing: MqttRoutingService) {
-        debugger
         let host = environment.mqttServer.url
         let wsport = environment.mqttServer.port
         let idTransaccion = uuidv4();
