8aba489cfa012383d391f5cfeab4a7869f972f22
developer externo <developerexterno@gmail.com>
2023-08-03 18:05:10 -0500

    cambios agregando clase RequestTrip

---
+++ src/app/modules/request-trip/data/request.ts
@@ -0,0 +1,44 @@
+
+// Define the class for the "point" object within "addresses"
+export class Point {
+    type: string;
+    coordinates: number[];
+    constructor() {
+    }
+  }
+  
+  // Define the class for the "addresses" object
+  export class Address {
+    sort: number;
+    maker: string;
+    addressStreet: string;
+    point: Point;
+    alias: string;
+    floor: string;
+    reference: string;
+    phone: string;
+
+  }
+  
+  // Define the class for the "payment" object
+  export class Payment {
+    amount: { value: number };
+    method: { type: string };
+  }
+  
+  // Define the main class with all the properties
+  export class RequestTrip {
+    id: number;
+    uuid: string;
+    status: string;
+    createdAt: number;
+    deliveryPrice: number;
+    tip: number;
+    description: string;
+    payment: Payment;
+    addresses: Address[];
+  
+  
+  }
+  
+  
--- src/app/modules/request-trip/request-trip.component.ts
+++ src/app/modules/request-trip/request-trip.component.ts
@@ -111,4 +111,7 @@ export class RequestTripComponent implements OnInit, AfterViewInit {
       });
   
     }
+    onSaveOrder(){
+      
+    }
    }
