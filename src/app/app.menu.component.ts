d27bc008e5d591119b76eeae0d05670efc2c5c7f
Cristian-nexus <cristian.velasquez@nexus-virtual.net>
2023-08-30 13:43:55 -0500

    menu option queja

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -23,7 +23,7 @@ export class AppMenuComponent implements OnInit {
             { label: 'Solicitar Viaje', icon: 'pi pi-fw pi-car', routerLink: ['/request-trip']},
             { label: 'Servicios en curso', icon: 'pi pi-fw pi-history', routerLink: ['/order-course']},
             { label: 'Historial de Órdenes', icon: 'pi pi-fw pi-history', routerLink: ['/order-history']},
-            { label: 'Call Center', icon: 'pi pi-fw pi-box', routerLink: ['/complaint-report']}
+            { label: 'Quejas', icon: 'pi pi-fw pi-box', routerLink: ['/complaint-report']}
         ];
         this.getProducts()
     }
