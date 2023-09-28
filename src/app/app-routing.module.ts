d823dc6622430f4f5a849899cb40474e98449b18
jhon larru <jhon.larru@siedevs.com>
2023-09-27 23:29:05 -0500

    clear routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import { DashboardDemoComponent } from './demo/view/dashboarddemo.component';
-import { FormLayoutDemoComponent } from './demo/view/formlayoutdemo.component';
-import { FloatLabelDemoComponent } from './demo/view/floatlabeldemo.component';
-import { InvalidStateDemoComponent } from './demo/view/invalidstatedemo.component';
-import { InputDemoComponent } from './demo/view/inputdemo.component';
-import { ButtonDemoComponent } from './demo/view/buttondemo.component';
-import { TableDemoComponent } from './demo/view/tabledemo.component';
-import { ListDemoComponent } from './demo/view/listdemo.component';
-import { TreeDemoComponent } from './demo/view/treedemo.component';
-import { PanelsDemoComponent } from './demo/view/panelsdemo.component';
-import { OverlaysDemoComponent } from './demo/view/overlaysdemo.component';
-import { MediaDemoComponent } from './demo/view/mediademo.component';
-import { MenusDemoComponent } from './demo/view/menusdemo.component';
-import { MessagesDemoComponent } from './demo/view/messagesdemo.component';
-import { MiscDemoComponent } from './demo/view/miscdemo.component';
-import { EmptyDemoComponent } from './demo/view/emptydemo.component';
-import { ChartsDemoComponent } from './demo/view/chartsdemo.component';
-import { FileDemoComponent } from './demo/view/filedemo.component';
-import { DocumentationComponent } from './demo/view/documentation.component';
-import { DisplayComponent } from './utilities/display.component';
-import { ElevationComponent } from './utilities/elevation.component';
-import { FlexboxComponent } from './utilities/flexbox.component';
-import { GridComponent } from './utilities/grid.component';
-import { IconsComponent } from './utilities/icons.component';
-import { WidgetsComponent } from './utilities/widgets.component';
-import { SpacingComponent } from './utilities/spacing.component';
-import { TypographyComponent } from './utilities/typography.component';
-import { TextComponent } from './utilities/text.component';
-
 import { AppMainComponent } from './app.main.component';
 import { AppNotfoundComponent } from './pages/app.notfound.component';
 import { AppErrorComponent } from './pages/app.error.component';
 import { AppAccessdeniedComponent } from './pages/app.accessdenied.component';
-import { AppCrudComponent } from './pages/app.crud.component';
-import { AppCalendarComponent } from './pages/app.calendar.component';
-import { AppTimelineDemoComponent } from './pages/app.timelinedemo.component';
 import { IsAuthenticated, IsNotAuthenticated } from './utils/auth-guard';
 import { SignInComponent } from './login/sign-in/sign-in.component';
-import { RequestTripComponent } from './modules/request-trip/request-trip.component';
 
 const routes: Routes = [
     {
