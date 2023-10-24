e7c37eb6a14133ff2f2f47f879bc3be3ba85786d
jhon larru <jhon.larru@siedevs.com>
2023-10-24 11:47:02 -0500

    solved circle

--- src/app/utils/circle/circleprogress.component.ts
+++ src/app/utils/circle/circleprogress.component.ts
@@ -13,8 +13,8 @@ export class CircleProgress implements OnInit, AfterViewInit {
     secondsElapsed: number = 0;
     minutesElapsed: number = 0;
 
-    @Input() readyToDmAt: number;
-    @Input() acceptAgentStoreAt: number;
+    @Input() endTime: number;
+    @Input() startTime: number;
 
     ngOnInit(): void {
         // this.secondsDone=this.minutes*60
@@ -22,19 +22,29 @@ export class CircleProgress implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-        this.totalTimeInSeconds = (this.readyToDmAt - this.acceptAgentStoreAt) / 1000;
-        this.start();
-      }
+      console.log("startTime",this.startTime)
+      console.log("endTime",this.endTime)
+      console.log("this.startTime - this.endTime",this.endTime - this.startTime)
+      this.totalTimeInSeconds = (this.endTime - this.startTime);
+      console.log("totalTimeInSeconds",this.totalTimeInSeconds)
+      this.start();
+    }
 
     start() {
         const progressEndValue = 100;
         const speed = 1000;
     
         const progress = setInterval(() => {
-            this.secondsElapsed++;
+            
+            const now=Number(new Date().getTime().toString().substring(0,10))
+            console.log("now",now)
+            
+            this.secondsElapsed=(this.endTime-now)
+            console.log("this.secondsElapsed",this.secondsElapsed)
             const progressPercentage = (this.secondsElapsed / this.totalTimeInSeconds) * progressEndValue;
+            console.log("progressPercentage",progressPercentage)
             this.minutesElapsed = Math.floor(this.secondsElapsed / 60);
-      
+            console.log("this.minutesElapsed",this.minutesElapsed)
             this.progressValue.nativeElement.textContent = `${this.minutesElapsed} min`;
             this.circularProgress.nativeElement.style.background = `conic-gradient(#d94545 ${progressPercentage * 3.6}deg, #ededed 0deg)`;
       
