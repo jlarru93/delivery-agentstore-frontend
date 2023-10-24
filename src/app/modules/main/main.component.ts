0a31b0c1919190de9f9fdd9b84d8e574361b4ef1
jhon larru <jhon.larru@siedevs.com>
2023-10-24 17:10:27 -0500

    extractLicenses

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -479,6 +479,6 @@ import { NgxPrinterService } from "ngx-printer";
     @ViewChild('PrintTemplate') private PrintTemplateTpl: TemplateRef<any>;
 
     printTemplate() {
-      this.printerService.printAngular(this.PrintTemplateTpl);
+      this.printerService.printAngular(this.PrintTemplateTpl)
     }
 }
