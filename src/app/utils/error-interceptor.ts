db10266bc1ea91dfb0738c7cdf9f8358469fa9bf
jhon larru <jhon.larru@siedevs.com>
2024-04-25 16:30:10 -0500

    message conection

--- src/app/utils/error-interceptor.ts
+++ src/app/utils/error-interceptor.ts
@@ -1,30 +1,24 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, throwError } from "rxjs";
-import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
 import { catchError } from 'rxjs/operators';
+import { ConnectionService } from "../modules/service/connection.service";
 //import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    //@BlockUI() blockUI: NgBlockUI;
-    constructor(private authenticationService : AuthService){
+    constructor(private readonly connectionService:ConnectionService){
 
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
        
-        return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.resetSecurityObject();
-                //location.reload();
-               
+        return next.handle(request).pipe(catchError((err:HttpErrorResponse) => {
+            if(err.status===0){
+                this.connectionService.setValue(false)
             }
-      //      this.blockUI.stop();
-            const error = err.error.message || err.statusText;
-            return throwError(error);
+            throw err
         }))
     }
 
